b1da4b416fbf5803b93c0525f7f0482d812acc01
Mohamad_RA <mralmaahlol@gmail.com>
2022-06-03 14:47:27 +0300

    Fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,22 +13,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\City::factory(10)->create();
-        \App\Models\Admin::factory(10)->create();
-        \App\Models\User::factory(10)->create();
-        \App\Models\Question::factory(10)->create();
-        \App\Models\Answer::factory(10)->create();
-        \App\Models\Award::factory(10)->create();
-        \App\Models\Type::factory(10)->create();
-        \App\Models\Place::factory(10)->create();
-        \App\Models\Option::factory(10)->create();
-        \App\Models\AwardPlace::factory(10)->create();
-        \App\Models\Experience::factory(10)->create();
-        \App\Models\Booking::factory(10)->create();
-        \App\Models\Plan::factory(10)->create();
-        \App\Models\FeatureTitle::factory(10)->create();
-        \App\Models\Feature::factory(10)->create();
-        \App\Models\FeaturePlace::factory(10)->create();
-        \App\Models\PlanContent::factory(10)->create();
+        $this->call([
+            CitySeeder::class,
+            AdminSeeder::class,
+            UserSeeder::class,
+            QuestionSeeder::class,
+            AnswerSeeder::class,
+            AwardSeeder::class,
+            TypeSeeder::class,
+            PlaceSeeder::class,
+            OptionSeeder::class,
+            AwardPlaceSeeder::class,
+            ExperienceSeeder::class,
+            BookingSeeder::class,
+            PlanSeeder::class,
+            FeatureTitleSeeder::class,
+            FeatureSeeder::class,
+            FeaturePlaceSeeder::class,
+            PlanContentSeeder::class,
+        ]);
     }
 }
