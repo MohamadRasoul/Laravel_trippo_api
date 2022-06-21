56a36dc945b2f174f5471522faa1005a6136df5b
Mohamad_RA <mralmaahlol@gmail.com>
2022-06-22 01:28:48 +0300

    add clear-cache api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use App\Http\Controllers\Api\ImageController;
 
 
 Route::post('image/upload',[ImageController::class,'uploadImage']);
+
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
